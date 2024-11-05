90c36c043169f7b335ef1cd4a89e0b005e0e03a7
Antti Haavikko <antti.haavikko@gmail.com>
2024-11-05 17:56:19 +0200

    Add collectible types

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -1,7 +1,21 @@
+using UnityEngine;
+
 public class Collectable : Clickable
 {
+    [SerializeField] private CollectibleType type;
+    
     public override void Click(Inventory inventory)
     {
         gameObject.SetActive(false);
+        if (type == CollectibleType.Battery) inventory.Batteries++;
+        if (type == CollectibleType.Pearl) inventory.Pearls++;
+        if (type == CollectibleType.Key) inventory.Keys++;
     }
+}
+
+public enum CollectibleType
+{
+    Pearl,
+    Battery,
+    Key
 }
