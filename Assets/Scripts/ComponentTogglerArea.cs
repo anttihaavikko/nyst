dedb1883033f562646da7580a48ae7b07070b8e0
Antti Haavikko <antti.haavikko@gmail.com>
2024-11-18 11:07:35 +0200

    Add light toggling

---
+++ Assets/Scripts/ComponentTogglerArea.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AnttiStarterKit.ScriptableObjects;
+using UnityEngine;
+
+public class ComponentTogglerArea : MonoBehaviour
+{
+    [SerializeField] private List<Behaviour> components;
+    [SerializeField] private bool defaultState;
+    [SerializeField] private SoundComposition sound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && components.Any())
+        {
+            sound?.Play(components[0].transform.position);
+            components.ForEach(c => c.enabled = !defaultState);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && components.Any())
+        {
+            sound?.Play(components[0].transform.position);
+            components.ForEach(c => c.enabled = defaultState);
+        }
+    }
+}
